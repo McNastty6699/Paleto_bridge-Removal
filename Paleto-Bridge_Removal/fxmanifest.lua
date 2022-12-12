97b4ed6b083989797fcd30f16cd874f7512fe97d
Kody Thompson <88422603+McNastty6699@users.noreply.github.com>
2022-12-11 18:10:41 -0600

    Add files via upload

---
+++ Paleto-Bridge_Removal/fxmanifest.lua
@@ -0,0 +1,12 @@
+name "Paleto Beach"
+author "McNastty6699"
+description "Custom Paleto Beach V1.0"
+
+fx_version 'cerulean'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+server_scripts {
+	'server.lua',
+}
---
+++ Paleto-Bridge_Removal/server.lua
@@ -0,0 +1,3 @@
+print('\n\n- - - - - - - - - - - - - - - - - - - - - -\n^7[^PALETO BEACH^7] ^7[^CUSTOM MAPP CREATED BY MCNASTTY#1922^7] ^PALETO BEACH.^0\n- - - - - - - - - - - - - - - - - - - - - -\n')
+           
+      
